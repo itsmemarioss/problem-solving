d107788ab464bcc651d71c4910840383b9aaf1e9
Mário Santos <super.mario.santos@gmail.com>
2018-06-23 12:19:39 -0300

    adding hackerrank interview preparation problems

--- backtracking/PasseioDoCavalo.java
+++ backtracking/PasseioDoCavalo.java
@@ -1,4 +1,4 @@
-package tentativaeerro;
+package backtracking;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
---
+++ com/hackerrank/interviewpreparation/sorting/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.hackerrank.interviewpreparation.sorting;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class BubbleSort {
+
+	public static void main(String[] args) {
+		int[] array = {2,5,1,67,23,60,9,10};
+		
+		sort(array);
+		
+		IntStream.of(array).forEach(i -> System.out.println(i));
+
+	}
+
+	private static int[] sort(int[] array) {
+		boolean isSorted = false;
+		
+		while(!isSorted) {	
+			isSorted = true;
+			for (int i = 0; i < array.length -1; i++) {
+				if(array[i]>array[i+1]) {
+					swap(array,i,i+1);
+					isSorted = false;
+				}
+			}
+			
+		}
+		return array;
+	}
+
+	private static void swap(int[] array, int i, int j) {
+		int aux = array[j];
+		array[j]=array[i];
+		array[i]=aux;
+	}
+
+}
