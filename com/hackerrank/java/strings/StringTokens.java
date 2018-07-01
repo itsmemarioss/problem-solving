decb43f27b2aea7634c25eba0df27329365d100f
Mário Santos <super.mario.santos@gmail.com>
2018-07-01 09:18:07 -0300

    String Tokens and regex

---
+++ com/hackerrank/java/strings/StringTokens.java
@@ -0,0 +1,31 @@
+package com.hackerrank.java.strings;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * https://www.hackerrank.com/challenges/java-string-tokens/problem
+ * 
+ * Difficulty: Easy
+ * 
+ * @author mario
+ *
+ */
+public class StringTokens {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		// Write your code here.
+		if (!s.trim().isEmpty()) {
+			String[] splited = s.trim().split("[ !,?._'@]+");
+			System.out.println(splited.length);
+			for (int i = 0; i < splited.length; i++) {
+				System.out.println(splited[i]);
+			}
+		}else {
+			System.out.println(0);
+		}
+		scan.close();
+	}
+}
