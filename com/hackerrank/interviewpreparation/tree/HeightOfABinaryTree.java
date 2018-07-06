de911d3a6680a6e869c81c26723a05ef66409cb0
Mário Santos <mario.santos@ufca.edu.br>
2018-07-06 12:00:15 -0300

    Heigh of a binary tree

---
+++ com/hackerrank/interviewpreparation/tree/HeightOfABinaryTree.java
@@ -0,0 +1,67 @@
+package com.hackerrank.interviewpreparation.tree;
+
+import java.util.*;
+import java.io.*;
+
+
+
+public class HeightOfABinaryTree {
+
+	static class Node {
+		Node left;
+		Node right;
+		int data;
+
+		Node(int data) {
+			this.data = data;
+			left = null;
+			right = null;
+		}
+	}
+	
+	/*
+	 * class Node int data; Node left; Node right;
+	 */
+	public static int height(Node root) {
+		// Write your code here.
+		if (root == null || (root.left == null && root.right == null)) {
+			return 0;
+		} else {
+			int leftValue = height(root.left);
+			int rightValue = height(root.right);
+			int max = leftValue > rightValue ? leftValue : rightValue;
+
+			return 1 + max;
+		}
+
+	}
+
+	public static Node insert(Node root, int data) {
+		if (root == null) {
+			return new Node(data);
+		} else {
+			Node cur;
+			if (data <= root.data) {
+				cur = insert(root.left, data);
+				root.left = cur;
+			} else {
+				cur = insert(root.right, data);
+				root.right = cur;
+			}
+			return root;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int t = scan.nextInt();
+		Node root = null;
+		while (t-- > 0) {
+			int data = scan.nextInt();
+			root = insert(root, data);
+		}
+		scan.close();
+		int height = height(root);
+		System.out.println(height);
+	}
+}
