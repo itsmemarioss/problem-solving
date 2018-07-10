6fb172f8d251cf27b64cf1ee2f5460e9194d7ef3
Mário Santos <mario.santos@ufca.edu.br>
2018-07-10 17:27:45 -0300

    geeksforgeeks

---
+++ org/geeksforgeeks/practice/PermutationsOfAGivenString.java
@@ -0,0 +1,12 @@
+package org.geeksforgeeks.practice;
+
+/**
+ * https://practice.geeksforgeeks.org/problems/permutations-of-a-given-string/0
+ * @author mario
+ *
+ */
+public class PermutationsOfAGivenString {
+	public static void main(String[] args) {
+		
+	}
+}
